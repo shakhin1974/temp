ae80810c0f49d1105f58d9120ffd12453b4ab282
shakhin1974 <57936556+shakhin1974@users.noreply.github.com>
2024-11-28 11:09:03 +0300

    temp.cpp

--- temp/temp.cpp
+++ temp/temp.cpp
@@ -34,6 +34,42 @@ int main()
 	Car BMW("BMW", "Mini", "Серый", 2020, 150, 200);
 	BMW.Vivod();
 
+    multimap<string, string> m;
+
+   // m.emplace(make_pair(("a"), ("aa")));
+   //m.emplace(make_pair("b", "ab"));
+   
+    m.emplace("b", "ab");
+    m.emplace("b", "abba");
+    m.emplace("a", "aa");
+    m.emplace("a", "aaa");
+    m.emplace("d", "ddd");
+    m.emplace("d", "DDD");
+    
+    for (const auto & p : m)
+    {
+     std::cout << p.first  << " => " << p.second << '\n';
+    }
+   multimap<string, string> mm;
+    mm.insert({ "1", "Red" });
+    mm.insert({ "1", "Blue" });
+    
+    multimap<string, string> mp1 = 
+    { { "Man", "Singing" },
+        { "Man", "Dancing" } };
+    mp1.insert({ { "Sal", "Reading" },
+                     { "Soya", "Music" },
+                     { "Sal", "Painting" },
+                     { "Sal", "Arts" } });
+    string k = "Man";
+    
+auto range = mp1.equal_range(k);
+std::cout   << k <<  " = " ; 
+for (auto i = range.first; i != range.second; ++i)
+{
+std::cout << i->second << " ";
+}
+std::cout << endl;
 
 }
 
